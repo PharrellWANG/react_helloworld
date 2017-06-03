1b37469c7b2cc9338df732b5e8666d8b0444bc2e
PharrellWANG <15113029g@connect.polyu.hk>
2017-06-03 18:27:32 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Welcome name="Sara"/>
+                <Welcome name="Cahal"/>
+                <Welcome name="Edite"/>
+            </div>
+        );
+    }
 }
 
 export default App;
